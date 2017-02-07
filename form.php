9ebd91f2dd4aaeba4415ae92319c60120c988ace
Martynas <martynas.gudai@gmail.com>
2017-02-07 08:04:50 -0800

    Uploading hoal wesites first version

---
+++ form.php
@@ -0,0 +1,16 @@
+﻿<?php	
+
+	$name = $_POST['name'];	
+
+	$phone = $_POST['phone'];	
+
+	$email = $_POST['email'];		
+
+	$to = "uabapskaitatau@gmail.com";	
+
+	$subject = "Užsakymas";	
+
+	mail($to, $subject, "Įmonės pavadinimas: " . $name . " telefono numeris: " . $phone . " el. paštas: " . $email);
+
+	echo "<script>window.location = 'http://zaibas.netau.net'</script>";
+?>
